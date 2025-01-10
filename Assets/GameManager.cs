da0eb57d73e4da11c236e5d32a0ff403323d4d01
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-01-10 15:55:37 +0100

    no_R

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -226,7 +226,7 @@ public class GameManager : MonoBehaviour
 
     void ActivateUltimate()
     {
-        if (Input.GetKeyDown(KeyCode.R) && mana >= manaMax && !ultimateActive)
+        if mana >= manaMax && !ultimateActive)
         {
             ultimateActive = true;
             mana = 0;
